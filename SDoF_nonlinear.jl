166f8c43e224573dfd72e8842a0d6fb6ecc10343
Kuts Michael <kutsjuice@gmail.com>
2023-03-08 23:18:20 +0300

    damping and frequency relation modified

--- SDoF_nonlinear.jl
+++ SDoF_nonlinear.jl
@@ -6,14 +6,14 @@ t_span = [0., 5.];
 t = t_span[1]:dt:t_span[2]
 
 # define damping constants and time relation
-m = 2; Dₐ = 0.004; kᵧ = 60
-damp(x) = Dₐ*(1 - (x/kᵧ)^m);
+m = 0.2; Dₐ = 0.004; kᵧ = 0.8
+damp(x) = Dₐ*(exp(-x/kᵧ)+1);
 
 ζ = damp.(t);
 
 # define frequenci constants and time relation
-kf = -0.7; shift = 50;
-freq(x) = exp(kf*x) + shift;
+kf = 1.7;  f_amp = 4.5; shift = 50 - f_amp;
+freq(x) = shift + f_amp*(1 - exp(-kf*x));
 
 f = freq.(t);
 
@@ -24,4 +24,24 @@ A = 100 .* exp.(-ζ .* ω .* t);
 
 y = A .* sin.(ϕ)
 
-plot(t,y, linewidth = 1.0); plot(t,A, "--k", linewidth = 2.0 )
+# plot(t,y, linewidth = 1.0); plot(t,A, "--k", linewidth = 2.0 )
+subplot(3,2,1)
+plot(A, ζ)
+# ylim([0.003,0.005])
+xscale("log")
+yscale("log")
+subplot(3,2,2)
+plot(t,ζ)
+yscale("log")
+
+subplot(3,2,3)
+plot(A, f)
+xscale("log")
+
+subplot(3,2,4)
+plot(t,f)
+
+subplot(3,1,3)
+plot(t,y)
+gcf().set_size_inches(12,8)
+tight_layout()
