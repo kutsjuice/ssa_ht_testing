82f7e01a6a1d43505f31232cc3cf0d64413ea228
Kuts Michael <kutsjuice@gmail.com>
2023-03-09 18:29:20 +0300

    frequency calculation added

--- SDoF_nonlinear.jl
+++ SDoF_nonlinear.jl
@@ -1,5 +1,29 @@
 using PyPlot;
 
+function diff14(vec::AbstractVector{<: Number}, time_step::Number)
+    der = similar(vec);
+    s_coefs = [1/12, -2/3, 2/3, -1/12];
+    f_coefs = [-25/12, 4, -3, 4/3, -1/4];
+    b_coefs = [1/4, -4/3, 3, -4, 25/12];
+    N = size(vec, 1);
+    M = size(vec, 2);
+    # almost all points calculates using midpoint scheme
+    for i = 3:(length(vec)-2)
+        der[i, :] = (vec[i-2,:].*s_coefs[1] + vec[i-1,:].*s_coefs[2] + vec[i+1,:].*s_coefs[3] + vec[i+2,:].*s_coefs[4]) /time_step;
+    end
+    # first points are calculated using forward scheme of the same order
+    for i = 1:2
+        der[i,:] = sum(hcat([vec[i+j-1,:].*f_coefs[j] for j in eachindex(f_coefs)]...), dims =2)./time_step;
+    end
+    # last points are calculated using bacckward scheme of the same order
+    for j in 1:M
+        for i = N-1:N
+            der[i,j] = (vec[i-4:i, j]'*b_coefs)/time_step;
+        end
+    end
+    return der;
+end
+
 # simulation interval
 dt = 1.0e-4;
 t_span = [0., 5.];
@@ -22,7 +46,13 @@ f = freq.(t);
 
 A = 100 .* exp.(-ζ .* ω .* t);
 
-y = A .* sin.(ϕ)
+y = A .* sin.(ϕ);
+ỹ = A .* cos.(ϕ); # to obtain imagine part of the signal we compute it directly to avoid using of HT 
+
+Ã = [sqrt(y[i]^2 + ỹ[i]^2) for i in eachindex(y)];
+
+ẏ = diff14(y, dt);
+
 
 # plot(t,y, linewidth = 1.0); plot(t,A, "--k", linewidth = 2.0 )
 subplot(3,2,1)
@@ -45,3 +75,27 @@ subplot(3,1,3)
 plot(t,y)
 gcf().set_size_inches(12,8)
 tight_layout()
+
+
+full = y + ỹ * im
+
+function frequency(z, dt)
+    y = real(z);
+    ỹ = imag(z);
+    ẏ = diff14(y, dt);
+    ỹ̇ = diff14(ỹ, dt);
+
+    A_sq = [(y[i]^2 + ỹ[i]^2) for i in eachindex(z)];
+
+    ω = similar(y);
+    for i in eachindex(ω)
+        ω[i] = (y[i]*ỹ̇[i] - ẏ[i]*ỹ[i])/A_sq[i];
+    end
+    return ω
+end
+
+
+
+
+ŵ = frequency(full, dt);
+plot(t,ω);plot(t, -ŵ, "--")
